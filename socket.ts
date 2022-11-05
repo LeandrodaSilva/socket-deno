6654af79135262c8c188aee5594ac7d5d7e6edf5
leandrodasilva <leandro.silva@euconquisto.com>
2022-11-05 20:21:23 -0300

    chore: split servers

---
+++ socket.ts
@@ -0,0 +1,14 @@
+import { WebSocketClient, WebSocketServer } from "https://deno.land/x/websocket@v0.1.4/mod.ts";
+
+const wss = new WebSocketServer(8080);
+const clients: WebSocketClient[] = [];
+
+wss.on("connection", function (ws: WebSocketClient) {
+  clients.push(ws);
+  ws.on("message", function (message: string) {
+    console.log(message);
+    clients.forEach((client) => {
+      client.send(message);
+    });
+  });
+});
