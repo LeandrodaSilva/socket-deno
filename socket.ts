04bbc0d64427b962d5183ad9affb35d1240d739b
leandrodasilva <leandro.silva@euconquisto.com>
2022-11-26 14:28:09 -0300

    feat: added fauna

--- socket.ts
+++ socket.ts
@@ -1,107 +1,120 @@
 import { WebSocketClient, WebSocketServer } from "https://deno.land/x/websocket@v0.1.4/mod.ts";
-// import "npm:mssql";
-// import {Request, Connection} from "./tedious/lib/tedious.js";
-// import { createRequire } from "https://deno.land/std@0.165.0/node/module.ts";
-// import { serveTls, serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-// const require = createRequire(import.meta.url);
-// const {Request, Connection} = require("./tedious/lib/tedious.js");
 const wss = new WebSocketServer(8080);
 const clients: WebSocketClient[] = [];
 
-// var config = {
-//   "server": "leproj-sql.database.windows.net",
-//   "authentication": {
-//     "type": "default",
-//     "options": {
-//       "userName": "leproj.admin",
-//       "password": "79RdDJt779PMMuw"
-//     }
-//   },
-//   "options": {
-//     "port": 1433,
-//     "database": "leproj_db",
-//     "trustServerCertificate": true
-//   }
-// }
-//
-// const sqlConfig = {
-//   user: "leproj.admin",
-//   password: "79RdDJt779PMMuw",
-//   database: "leproj_db",
-//   server: 'leproj-sql.database.windows.net',
-//   options: {
-//     encrypt: true, // for azure
-//     trustServerCertificate: true // change to true for local dev / self-signed certs
-//   }
-// }
-//
-// await serve(async (_req) => {
-//   try {
-//     // console.log(sql)
-//     // make sure that any items are correctly URL encoded in the connection string
-//     // await sql.connect(sqlConfig)
-//     // const result = await sql.query`select * from Messages`
-//     const connection = new Connection(config);
-//     connection.on('connect', (err: any) => {
-//       if (err) {
-//         console.log('Connection Failed');
-//         throw err;
-//       }
-//       executeStatement();
-//     });
-//     connection.connect();
-//     function executeStatement() {
-//       const request = new Request("select * from Messages", (err: any, rowCount: number) => {
-//         if (err) {
-//           throw err;
-//         }
-//         console.log('DONE!');
-//         connection.close();
-//       });
-//       // Emits a 'DoneInProc' event when completed.
-//       request.on('row', (columns: Array<{value: null | string}>) => {
-//         columns.forEach((column) => {
-//           if (column.value === null) {
-//             console.log('NULL');
-//           } else {
-//             console.log(column.value);
-//             console.dir(column.value)
-//             return new Response(column.value, {
-//               headers: { "content-type": "text/plain" },
-//             });
-//           }
-//         });
-//       });
-//       // In SQL Server 2000 you may need: connection.execSqlBatch(request);
-//       connection.execSql(request);
-//     }
-//   } catch (err) {
-//     console.log(err)
-//     return new Response(err.toString(), {
-//       headers: { "content-type": "text/plain" },
-//     });
-//   }
-//   return new Response("error", {
-//     headers: { "content-type": "text/plain" },
-//   });
-// }, {
-//   // certFile: "./server.crt",
-//   // keyFile: "./server.key",
-// });
-
 wss.on("connection", function (ws: WebSocketClient) {
   clients.push(ws);
-  ws.on("message", function (message: string | Uint8Array) {
-    fetch("https://chat.leandrodasilva.dev/api/message", {
-      method: "POST",
-      headers: {
-        "Content-Type": "text/plain",
-      },
-      body: message,
-    })
+  ws.on("message", async function (message: string) {
+    await createMessage(
+      JSON.parse(message) as { data: string; author: string },
+    );
     clients.forEach((client) => {
       client.send(message);
     });
   });
 });
+
+/** Create a new quote in the database. */
+async function createMessage({ data, author, }: {
+  data: string;
+  author: string;
+}): Promise<{ data?: string; author?: string; errors?: FaunaError[] }> {
+  const query = `
+    mutation($data: String!, $author: String!) {
+      createMessage(data: { data: $data, author: $author }) {
+        _id
+        data
+        author
+      }
+    }
+  `;
+
+  const { data: queryData, errors } = await queryFauna(query, { data, author });
+  if (errors) {
+    return { errors };
+  }
+
+  const { createMessage } = queryData as {
+    createMessage: {
+      data: string;
+      author: string;
+    };
+  };
+
+  return createMessage; // {data: "*", author: "*"}
+}
+
+/** Get all quotes available in the database. */
+async function getAllMessages() {
+  const query = `
+    query {
+      allMessages {
+        data {
+          data
+          author
+        }
+      }
+    }
+  `;
+
+  const { data, errors } = await queryFauna(query, {});
+  if (errors) {
+    return { errors };
+  }
+
+  const {
+    allMessages: { data: messages },
+  } = data as { allMessages: { data: string[] } };
+
+  return { messages };
+}
+
+
+
+type FaunaError = {
+  message: string;
+};
+
+
+/** Query FaunaDB GraphQL endpoint with the provided query and variables. */
+async function queryFauna(
+  query: string,
+  variables: { [key: string]: unknown },
+): Promise<{
+  data?: unknown;
+  errors?: FaunaError[];
+}> {
+  // Grab the secret from the environment.
+  const token = Deno.env.get("FAUNA_SECRET");
+  if (!token) {
+    throw new Error("environment variable FAUNA_SECRET not set");
+  }
+
+  try {
+    // Make a POST request to fauna's graphql endpoint with body being
+    // the query and its variables.
+    const res = await fetch("https://graphql.fauna.com/graphql", {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    });
+
+    const { data, errors } = await res.json();
+    if (errors) {
+      // Return the first error if there are any.
+      return { data, errors };
+    }
+
+    return { data };
+  } catch (error) {
+    console.error(error);
+    return { errors: [{ message: "failed to fetch data from fauna" }] };
+  }
+}
