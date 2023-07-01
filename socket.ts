e9d17342cfb672ee0487b3cf6ce8971ca62fbb0f
SA CRM <sa.crmmatriculas@example.com>
2023-07-01 13:19:12 -0300

    feat: Added clear database

--- socket.ts
+++ socket.ts
@@ -19,6 +19,12 @@ wss.on("connection", function (ws: WebSocketClient) {
   clients.push(ws);
   ws.on("message", async function (message: string) {
     const parsed: Message = JSON.parse(message);
+
+    if (parsed.type === 'text' && parsed.data === '/clear-database') {
+      await kv.clear();
+      return;
+    }
+
     const uuid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     const date = new Date().toISOString().split('T')[0];
     await kv.set(
